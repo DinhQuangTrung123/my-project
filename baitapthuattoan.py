b3c4eb7e7d773b4ec8ac98eec91d170b76101ebe
CodeXploreRePo <CodeXplore>
2021-10-30 03:42:18 +0700

    add new file

--- baitapthuattoan.py
+++ baitapthuattoan.py
@@ -1,4 +1,6 @@
 import re 
 str_name="OK - PON Temp #PON1: 34. PON2: 35. PON3: 35. PON4: 34. PON5: 36. PON6: 34. PON7: 40. PON8: 36. PON9: 36. PON10: 33. PON11: 36. PON12: 37. PON13: 34. PON14: 36. PON15: -. PON16: -."
 haddler_regex = re.findall("PON\d+:\s+\d+",str_name) 
+
 print(haddler_regex)
+
